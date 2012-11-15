120ae643f75d3e4ea387ffd48cf4bad44fcd59c6
Ali Aman Jalbani <ali.amaan@gmail.com>
2012-11-15 12:15:33 -0800

    more commits

---
+++ src/page/resume-page.php
@@ -0,0 +1,121 @@
+<?php
+?>
+<?php
+?>
+	   <div class="page-body resume-page">
+  	    <div class="page-body-biography">
+<!--	 		<p class='page-body-heading'>Objective</p>-->
+<!--			  I am seeking a Software Engineering opportunity specializing in Object Oriented Analysis, Design and Implementation. I am looking to apply my skills and experience on ambitious projects and like to be involved across all tiers of application development.-->
+			<p class='page-body-heading'>Summary</p>
+			  Nearly 7 years of experience in Object Oriented Software Development and Engineering, with special focus on open source web frameworks and ERPs. Lead Team of Technical Consultants on a global software solution for various financial institutions. 
+			  Worked on multiple projects on cross platform applications; experience in data migration for sensitive financial data. Have designed and developed across all tiers of web application development; adhere to coding and scripting standards.
+            <p class='page-body-heading'>Technical Skills</p>
+              <ul>
+			    <li>Skills:	Java   PHP,(Swing, Multi-Threading, Thread Concurrency, JDBC, JSP, Servlets), J2EE, EJB 2.0, C/C++.</li>
+				<li>Frameworks:    Drupal, Smarty, Code Igniter, Zend, Struts 1.2.9, Spring 2.5.6.</li>
+				<li>ORMs:		   Doctrine, Hibernate 3.5. </li>
+				<li>App Servers:   Apache Tomcat, Jboss.</li>
+				<li>Internet:	   HTML, DHTML, CSS, Javascript, SAX, DOM, XML, XSLT, AJAX Javascript libraries (JQuery 1.3.x, DOJO 1.1.1).</li>
+				<li>Microsoft:	   .NET Framework(C#, VB.NET, ASP.NET).</li>
+				<li>Engineering:   UML, Design Patterns (both J2EE &amp; J2SE), Anti-pattern risk mitigation.</li>
+				<li>Databases:	   SQL, PL-SQL, Oracle 9i, MS SQL Server, MySQL.</li>
+				<li>Software/IDEs: Eclipse, NetBeans, Visual Studio.</li>
+				<li>OS: 		   MS Windows, UNIX, Linux</li>
+				<li>Testing: 	   JUnit</li>
+			</ul>
+		  <p class='page-body-heading'>Work Experience</p>
+			TurnHere Internet Video	<br/>	  			     	 
+			Sr. Web Engineer<br/>	 			
+			Emeryville, CA<br/>	        
+			Mar 2009 – Till Date<br/>
+			<i>Engineering a platform for content management &amp; distribution for web advertisement</i>
+			<ul>
+				<li>Building a Web 2.0 service for enterprises to request internet video advertisements.</li>
+				<li>Used Open Source technologies on the LAMP stack.</li>
+				<li>Worked on CSS, JavaScript and PHP and PHP Frameworks (Drupal, Smarty, Zend, Doctrine).</li>
+				<li>Integrated various third party API's (Rhozet, BrightCove).</li>
+				<li>Developed Credit Card Transacaction Management (currently working towards PCI compliance).</li>
+				<li>Used MySQL in the backend.</li>
+				<li>Worked with JQuery for UI widgets (along with other various standard client side scripting tools and libraries).</li>
+			</ul>
+
+
+			Bay Area Air Quality Management District<br/>	     
+			Software Engineer<br/>
+			San Francisco, CA<br/>	 						       
+			Mar 2008 – Dec 2008<br/>
+			<i>Designing and developing the district’s new production system.</i>
+			<ul>
+				<li>Using UML for Object Oriented Design of new production system.</li>
+				<li>Used HTML, XHTML, DHTML, AJAX libraries (DOJO, Jquery).</li>
+				<li>Worked on CSS, JavaScript and PHP for extending the website (www.baaqmd.gov).</li>
+				<li>Used Javascript libraries to add effects to the website.</li>
+				<li>Involved in decision making process for technology selection and organized proof of concept exercise for the Java Team for their new system.</li>
+				<li>Dynamic PDF generation using FPDF.</li>
+				<li>Encrypting PII (Personally Identifiable Information).</li>
+				<li>Wrote use cases, DFD, Flowcharts, Use Case Sequence Diagrams, ERD, Class Diagrams, Wire Frames, Story Board Mockups.</li>
+			</ul>
+			
+			National Data Consultants<br/>		  			            
+			Technical Team Lead<br/>			
+			Karachi, Pakistan<br/>					        
+			Nov 2004 – Jan 2008<br/>	
+			<i>Designed and implemented financial software for banks &amp; financial institutions.</i> 
+			<ul>	 
+				<li>This was a web project developed on Java web frameworks.</li>
+				<li>Used Business Objects XI for reporting web intelligence documents.</li>
+				<li>Oracle Backend.</li>
+				<li>Perl scripting on *nix platofrms.</li>
+				<li>One of the more critical parts of implementing a project for a financial institution is to successfully execute data clean up and migration. This was done using data migration methodologies for systems on various platforms.</li> 
+				<li>Gathered requirements for the various banking modules for commercial banks. Involved in steering committees from inception to completion. Conducted UAT over more than 8 cities for over 600 users. Provided helpdesk support to debug/troubleshoot.</li>
+				<li>Engaged in the full software development life cycle; requirements specification, prototyping, design, implementation, testing and maintenance.</li>
+			</ul>
+			
+			Business Solutions<br/>						     
+			Software Engineer<br/>		  	
+			Karachi, Pakistan<br/>				 
+			May 2002 – Aug 2004<br/>	
+			<i>Designed and developed system for a scholarship granting NGO.</i>
+			<ul>
+				<li>.NET(C#, VB.NET and ASP.NET)</li>
+				<li>SQL Server.</li>
+				<li>Crystal Reports.</li>
+			</ul>
+			<p class='page-body-heading'>Projects</p>
+			    <ul>
+					<li><a href="http://www.turnhere.com">Turnhere Web Video Advertisement</a></li>
+					<li><a href="http://www.baaqmd.gov">Bay Area Air Quality Management District.</a></li>
+					<li>
+						<a href="http://www.temenos.com">Temenos T24 (J2EE based) Banking ERP</a><br/>
+						<i>
+							Customizing and Implementing Banking Modules on JAVA based ERP; T24 by Temenos, major financial modules including:
+							Teller, Funds Transfer, Money Market, Foreign Exchange, Mortgages, Loans and Deposits, Loan Scheduling and Repayment, Treasury, Securities, Local Developments Template Programming, Remittances, National Savings Certificates, Prize Bonds, Safe/Vault Management, SWIFT, Correspondent Banking, Reporting, Enquiries, Ledgers, Trial Balance, Financial Statements and Data Migration for various banks:
+						</i>
+						<ol>
+						  <li>JS Bank/ formerly American Express Pakistan</li>
+						  <li>State Bank of Pakistan</li>
+						</ol>
+					</li>
+					<li>
+						AKESP (Aga Khan Education Scholarship Program)<br/>
+						<ol>
+						  <li>Microsoft .NET based project. Designed reports of Crystal Reports.</li>
+						</ol>
+					</li>
+				</ul>
+				
+			<p class='page-body-heading'>Education</p>
+			  <ul>
+			    <li>
+			    	MCS (Masters in Computer Sceince)<br/>
+			    	SZABIST	Karachi, Pakistan<br/>
+			    	August 2005
+			    </li>
+			    <li>
+			    	BCS (Bachelors in Computer Sceince)<br/>
+			    	SZABIST	Karachi, Pakistan<br/>
+			    	May 2004
+			    </li>
+			  </ul>
+  	    </div>
+  	  </div>
